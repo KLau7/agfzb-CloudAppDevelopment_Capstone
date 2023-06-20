ee3d05f5557791a5f16a0eabfb4ae40bab82a437
klau7 <leonlau1998@gmail.com>
2023-06-19 20:52:59 -0400

    feat: stupid cloud functions

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -1,4 +1,14 @@
 /**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+ 
+/**
  * Get all dealerships
  */
 
@@ -6,16 +16,22 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-      const cloudant = CloudantV1.newInstance({
-          authenticator: authenticator
-      });
-      cloudant.setServiceUrl(params.COUCH_URL);
-      try {
-        let dbList = await cloudant.getAllDbs();
-        return { "dbs": dbList.result };
-      } catch (error) {
-          return { error: error.description };
-      }
+    const state = params.state;
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = CloudantV1.newInstance({
+        authenticator: authenticator
+    });
+    cloudant.setServiceUrl(params.COUCH_URL);
+    try {
+        let dbList = await cloudant.postAllDocs({
+            db: 'dealerships',
+            includeDocs: true,
+        });
+        let allDocs = dbList.result.rows.map(row => row.doc)
+        if (state) allDocs = allDocs.filter(doc => doc.state === state)
+        return { body: allDocs };
+    } catch (error) {
+        return { error: error.description };
+    }
 }
 
