a78fefdeffc73cb9751e5bd792e83e0c2f6fba11
klau7 <leonlau1998@gmail.com>
2023-06-19 22:43:48 -0400

    feat: admin site

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 # from .models import related models
-from models import CarMake, CarModel
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    search_fields = ['car_make']
+    extra = 3
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    search_fields = ['car_make', 'name', 'type', 'year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'description']
+    inlines = [CarModelInline]
 
 # Register models here
-admin.register()
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
