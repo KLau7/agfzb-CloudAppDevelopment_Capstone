077cbaca8f14d7e5376ec86b6b85388fbf5694ca
klau7 <leonlau1998@gmail.com>
2023-06-19 21:38:33 -0400

    feat: added models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,9 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(default='')
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,7 +22,21 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    TYPE_CHOICES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'WAGON')
+    ]
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    dealer_id = models.IntegerField()
+    type = models.CharField(
+        max_length=30,
+        choices=TYPE_CHOICES,
+        default='Sedan'
+    )
+    year = models.DateField()
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
